95313fa47173d5c1a9e6302deb94226bb5167089
Washipwas <albermg207@gmail.com>
2023-12-06 23:27:37 +0100

    Editar funciona correctamente

--- Todo/Practica2/src/gameRouter.js
+++ Todo/Practica2/src/gameRouter.js
@@ -12,23 +12,56 @@ router.get('/', (req, res) => {
 
 router.get('/new', (req, res) => {
 
-    res.render('PracticaWebPaginaNuevoElemento');
+    let tipo = "/";
+    let funcion = "/post/new";
+    res.render('PracticaWebPaginaNuevoElemento',{tipo,funcion});
 });
 
 router.get('/post/:id/edit', (req, res) => {
 
     let post = gameService.getPost(req.params.id);
-    res.render('PracticaWebPaginaNuevoElemento', { post });
+    let tipo = "/post/:id";
+    let funcion = "/post/edit";
+    let id = req.params.id;
+    gameService.saveId(id);
+    res.render('PracticaWebPaginaNuevoElemento',{post,tipo,funcion});
+});
+
+router.post('/post/edit', (req, res) => {
+
+    let {name, image, description, trailer, fecha, desarrolladora, genero, ps ,xbox, nswitch,pc} = req.body;
+    let symbolps = gameService.symbol(ps);
+    ps = gameService.symbolcolor(ps);
+    let symbolxbox = gameService.symbol(xbox);
+    xbox = gameService.symbolcolor(xbox);
+    let symbolswitch = gameService.symbol(nswitch);
+    nswitch = gameService.symbolcolor(nswitch);
+    let symbolpc = gameService.symbol(pc);
+    pc = gameService.symbolcolor(pc);
+    let accion = "editado";
+    let paginaName = "detalle";
+    let id = gameService.getId();
+    let pagina = "/post/" + id;
+    gameService.editPost(id,{name, image, description, trailer, fecha, desarrolladora, genero, ps, symbolps, xbox, symbolxbox, nswitch, symbolswitch, pc, symbolpc});
+    res.render('PaginaIntermedia',{name,accion,pagina,paginaName});
 });
 
 router.post('/post/new', (req, res) => {
 
-    let { name, image, description, trailer, fecha, desarrolladora, genero, consola } = req.body;
+    let {name, image, description, trailer, fecha, desarrolladora, genero, ps ,xbox, nswitch, pc} = req.body;
+    let symbolps = gameService.symbol(ps);
+    ps = gameService.symbolcolor(ps);
+    let symbolxbox = gameService.symbol(xbox);
+    xbox = gameService.symbolcolor(xbox);
+    let symbolswitch = gameService.symbol(nswitch);
+    nswitch = gameService.symbolcolor(nswitch);
+    let symbolpc = gameService.symbol(pc);
+    pc = gameService.symbolcolor(pc);
     let accion = "creado";
-
-    gameService.addPost({ name, image, description, trailer, fecha, desarrolladora, genero, consola });
-
-    res.render('PaginaIntermedia',{name, accion});
+    let paginaName = "principal";
+    let pagina = "/";
+    gameService.addPost({name, image, description, trailer, fecha, desarrolladora, genero, ps, symbolps, xbox, symbolxbox, nswitch, symbolswitch, pc, symbolpc});
+    res.render('PaginaIntermedia',{name,accion,pagina,paginaName});
 });
 
 router.get('/post/:id', (req, res) => {
@@ -38,16 +71,19 @@ router.get('/post/:id', (req, res) => {
     let {user, text} = req.body;
     gameService.addComment(post, {user, text});
 
-    res.render('PracticaWebPaginaDetalle', { post });
+    res.render('PracticaWebPaginaDetalle',{post});
 });
 
 router.get('/post/:id/delete', (req, res) => {
 
     let post = gameService.getPost(req.params.id);
+    let name = post.name;
     let accion = "eliminado";
+    let paginaName = "principal";
+    let pagina = "/";
     gameService.deletePost(req.params.id);
 
-    res.render('PaginaIntermedia',{post,accion});
+    res.render('PaginaIntermedia',{name,accion,pagina,paginaName});
 });
 
 export default router;
--- Todo/Practica2/src/gameService.js
+++ Todo/Practica2/src/gameService.js
@@ -1,20 +1,23 @@
 const posts = new Map();
+let idVariable;
 let nextId = 0;
 
-addPost({ name: "Red Dead Redemption 2", image: "/Imagenes_practica/RDR2.jpg", description: "Tras un asalto fallido en el pueblo de Blackwater, Arthur Morgan y la pandilla de Van der Linde se ven forzados a huir. Con agentes federales y los mejores cazarrecompensas pisando sus talones, la pandilla deberá asaltar, robar y hacerse camino a través de una América despiadada para poder sobrevivir. Mientras crecen las divisiones que amenazan con el fin de la pandilla Arthur debe decidir entre sus propios ideales y su lealtad a la pandilla que lo crió.",
- fecha:'26-10-2018', trailer:'https://www.youtube.com/embed/MyaYlbizpvs?si=VCBSoHWHA-EuwMwb', desarrolladora:'Rockstar', comments: [{user:"Sebas", text:"Si", reviewValue: [,]}, {user:"Alberto", text:"No", reviewValue: [,,,,]}, {user:"Jorge", text:"Tambien", reviewValue: [,,,]}]});
-addPost({ name: "Factorio", image:"/Imagenes_practica/Factorio.jpeg", description: "Pago bien"});
+addPost({
+    name: "Red Dead Redemption 2", image: "/Imagenes_practica/RDR2.jpg", description: "Tras un asalto fallido en el pueblo de Blackwater, Arthur Morgan y la pandilla de Van der Linde se ven forzados a huir. Con agentes federales y los mejores cazarrecompensas pisando sus talones, la pandilla deberá asaltar, robar y hacerse camino a través de una América despiadada para poder sobrevivir. Mientras crecen las divisiones que amenazan con el fin de la pandilla Arthur debe decidir entre sus propios ideales y su lealtad a la pandilla que lo crió.",
+    fecha: "2018-10-26", trailer: "https://www.youtube.com/embed/MyaYlbizpvs?si=VCBSoHWHA-EuwMwb", desarrolladora: "Rockstar", ps: "tick", comments: [{ user: "Sebas", text: "Si", reviewValue: [,] }, { user: "Alberto", text: "No", reviewValue: [, , , ,] }, { user: "Jorge", text: "Tambien", reviewValue: [, , ,] }]
+});
+addPost({ name: "Factorio", image: "/Imagenes_practica/Factorio.jpeg", description: "Pago bien" });
 addPost({ name: "Call of Duty", image: "/Imagenes_practica/COD.jpeg", description: "Barata, barata" });
 addPost({ name: "Grand theft auto V", image: "/Imagenes_practica/gtav.jpeg", description: "Pago bien" });
 addPost({ name: "Mario Galaxy", image: "/Imagenes_practica/MarioGalaxy.jpg", description: "Barata, barata" });
 addPost({ name: "God of War Ragnarok", image: "/Imagenes_practica/godofwar.jpeg", description: "Barata, barata" });
-addPost({ name: "The last of us", image:"/Imagenes_practica/thelastofus.jpeg", description: "Pago bien" });
-addPost({ name: "ARK", image:"/Imagenes_practica/ARK.jpeg", description: "Pago bien" });
-addPost({ name: "Bloodborne", image:"/Imagenes_practica/Bloodborne.jpeg", description: "Pago bien" });
-addPost({ name: "Dantes Inferno", image:"/Imagenes_practica/dantes.jpeg", description: "Pago bien" });
-addPost({ name: "Hades", image:"/Imagenes_practica/Hades.jpeg", description: "Pago bien" });
-addPost({ name: "Depprock", image:"/Imagenes_practica/depprock.jpeg", description: "Pago bien" });
-addPost({ name: "EA Sports 24", image:"/Imagenes_practica/fifa24.jpeg", description: "Pago bien" });
+addPost({ name: "The last of us", image: "/Imagenes_practica/thelastofus.jpeg", description: "Pago bien" });
+addPost({ name: "ARK", image: "/Imagenes_practica/ARK.jpeg", description: "Pago bien" });
+addPost({ name: "Bloodborne", image: "/Imagenes_practica/Bloodborne.jpeg", description: "Pago bien" });
+addPost({ name: "Dantes Inferno", image: "/Imagenes_practica/dantes.jpeg", description: "Pago bien" });
+addPost({ name: "Hades", image: "/Imagenes_practica/Hades.jpeg", description: "Pago bien" });
+addPost({ name: "Depprock", image: "/Imagenes_practica/depprock.jpeg", description: "Pago bien" });
+addPost({ name: "EA Sports 24", image: "/Imagenes_practica/fifa24.jpeg", description: "Pago bien" });
 addPost({ name: "Minecraft", image: "/Imagenes_practica/minecraft.jpeg", description: "Barata, barata" });
 addPost({ name: "Mafia City", image: "/Imagenes_practica/mafiacity.jpeg", description: "Pago bien" });
 
@@ -26,27 +29,53 @@ export function addPost(post) {
     posts.set(post.id, post);
 }
 
-export function deletePost(id){
+export function editPost(id, post) {
+    post.id = id;
+    posts.set(post.id, post);
+}
+
+export function saveId(id) {
+    idVariable = id;
+}
+
+export function getId() {
+    return idVariable;
+}
+
+export function deletePost(id) {
     posts.delete(id);
 }
 
-export function getPosts(){
+export function getPosts() {
     return [...posts.values()];
 }
 
-export function getPost(id){
+export function getPost(id) {
     return posts.get(id);
 }
 
-export function addComment(post, comment)
-{
-    if(comment.user != null)
-    post.comments.push(comment);
+export function symbol(console) {
+    if (console != "tick") {
+        return "bi bi-x-lg";
+    }
+
+    else {
+        return "bi bi-check2"
+    }
 }
 
-export function seleccionarGenero(){
-    let selectGen = document.getElementById('selectGen');
-    let genero = selectGen.value;    
+export function symbolcolor(console) {
+    if (console != "tick") {
+        return "tick-red";
+    }
 
-    document.getElementById("generoSeleccionado").innerText = `Has seleccionado ${genero}`;
+    else {
+        return "tick";
+    }
+}
+
+
+export function addComment(post, comment) {
+    if (comment.user != null)
+        post.comments.push(comment);
 }
